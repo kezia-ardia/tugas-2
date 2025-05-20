851123804c33b85dc8153c21e5617422e78ac98e
Kezia Ardia Sasaki <keziaardia03@gmail.com>
2025-05-20 19:41:24 +0800

    Add files via upload

---
+++ bebas.py
@@ -0,0 +1,17 @@
+#konditional statement
+# if else dalam satu line
+# x if kondisi else y
+
+# positif/negatif
+
+angka = 5
+umur = 18
+
+#positif/neatif
+print("positif" if angka > 0 else "negatif")
+
+# ganjil/genap
+print("ganjil" if angka % 2 == 0 else "genap")
+
+# dewasa/anak-anak
+print("dewasa" if umur >=18 else "anak-anak")
